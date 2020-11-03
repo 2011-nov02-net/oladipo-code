1062c840e8cec607daf51f678ffda689ec1b0846
Oladipo Osinubi <diposinubi@gmail.com>
2020-11-03 17:21:09 -0500

    added comments

--- cBasics/Program.cs
+++ cBasics/Program.cs
@@ -6,10 +6,13 @@ namespace cBasics
     {
         static void Main(string[] args)
         {
+            // ask for input from user
             Console.WriteLine("Please input a multi-word term:");
+            //convert input into capital letters and save as a variable
             string input = Console.ReadLine().ToUpper();
-            string[] words = input.Split(' ');
-            //words = ["central", "processing","unit"]
+            //convert input vairable into an array of strings
+            string[] words = input.Split(' ');//e.g words = ["CENTRAL", "PROCESSING","UNIT"]
+            //for each word in the array, write out the first letter concatinated
           foreach (var word in words)
         {
           Console.Write($"{word[0]}");
