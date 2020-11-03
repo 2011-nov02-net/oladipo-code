d140043d4e82323c60da0aa87b90709e769f2150
Oladipo Osinubi <diposinubi@gmail.com>
2020-11-03 16:11:09 -0500

    added cBasics file

---
+++ cBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
